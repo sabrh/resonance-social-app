8d5523a4d413d6141daec5542ffef36b4e878bd9
Rahat-khan117 <rahatkhan015002@gmail.com>
2025-09-17 18:45:11 +0600

    post box added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,7 +9,7 @@ const MainLayout: FC = () => {
     <div>
         <header><Navbar /></header>
         <main>
-            <div className='min-h-[calc(100vh-132px)] mx-8 md:mx-16'>
+            <div className='min-h-[calc(100vh-132px)] mx-4 md:mx-16'>
             <Outlet></Outlet>
             </div>
         </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import type { FC } from 'react';
 
 const Home: FC = () => {
   return (
-    <div>
-      Home page
+    <div className='mt-12 px-4 sm:px-7 md:px-12 lg:px-20 2xl:px-20 '>
+      {/* post section */}
+      <div className='w-full'>
+        <div></div>
+        {/* main post box */}
+        <div className='rounded-sm px-5 py-6'>
+          <button className='btn bg-blue-600 text-white'>News feed</button>
+          <div className='mt-3 shadow-2xl bg-[#f6ecec] rounded-xl px-4 py-4'>
+            <p className='text-xl font-bold'>Create New Post</p>
+            <input className='bg-white rounded-2xl h-[50px] w-full mt-3' type="text" />
+            <div className='mt-5 ml-6 flex gap-8'>
+              <p className='text-xl font-bold flex gap-2 items-center cursor-pointer'><i className="fa-solid fa-images text-4xl text-green-600"></i><span>Photos</span></p>
+              <p className='text-xl font-bold flex gap-2 items-center cursor-pointer'><i className="fa-regular fa-face-smile text-4xl text-yellow-600"></i><span>Feelings/Activity</span></p>
+
+              <button className='btn bg-green-600 text-white ml-7'>Post</button>
+              
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
