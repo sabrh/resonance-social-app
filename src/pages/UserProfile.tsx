0ebf215d2f8e2b700c602a4fdd5d313536e361e8
RGAntu <rgantu887@gmail.com>
2025-09-18 10:11:33 +0600

    added profile picture

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -4,13 +4,27 @@ const UserProfile: FC = () => {
   return (
     <div className="max-w-5xl mx-auto bg-white shadow rounded-lg overflow-hidden">
       {/* Banner Photo */}
-      <div className="relative h-48 bg-gradient-to-r from-blue-500 to-indigo-600">
+       <div className="relative h-48 bg-gradient-to-r from-blue-500 to-indigo-600">
         <img
           src="../../public/banner1.jpg"
           alt="Cover"
           className="w-full h-full object-cover"
         />
+        {/* Profile Picture */}
+        <div className="absolute -bottom-8 left-6">
+          <img
+            src="../../public/profile1.png"
+            alt="Profile"
+            className="w-32 h-32 rounded-full border-4 border-white object-cover"
+          />
+        </div>
       </div>
+
+
+      
+
+
+
     </div>
   );
 };
