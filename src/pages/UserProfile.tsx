1db1eb06c365884a4bffb86b0825b290380690b9
RGAntu <rgantu887@gmail.com>
2025-09-18 09:52:10 +0600

    added banner section

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import type { FC } from 'react';
+import type { FC } from "react";
 
 const UserProfile: FC = () => {
   return (
-    <div>
-      user profile page
+    <div className="max-w-5xl mx-auto bg-white shadow rounded-lg overflow-hidden">
+      {/* Banner Photo */}
+      <div className="relative h-48 bg-gradient-to-r from-blue-500 to-indigo-600">
+        <img
+          src="../../public/banner1.jpg"
+          alt="Cover"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   );
 };
